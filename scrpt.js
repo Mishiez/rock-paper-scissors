0d4549a69ad1c7e7385e4c62ee4ed2a09a81b34a
michelle <michellegathonimain@gmail.com>
2023-07-19 09:06:50 +0300

    Make the code look clean. Add some indentation and change the variable names into names that are nouns and addjectives

--- scrpt.js
+++ scrpt.js
@@ -3,73 +3,76 @@ function getComputerChoice(){
     return computerChoice;  
 }
 function getPlayerChoice(){
-  let playerChoice=prompt("Choose between rock paper and scissors: ")
-  return playerChoice.toUpperCase();
+    let playerChoice=prompt("Choose between rock paper and scissors: ")
+    return playerChoice.toUpperCase();
 }
 
 function playRound(playerSelection,computerSelection){
  
-  if(playerSelection==="ROCK"&&computerSelection==="Paper"){
-    return "You lose! Paper beats Rock";
+    if(playerSelection==="ROCK"&&computerSelection==="Paper"){
+      return "You lose! Paper beats Rock";
   }
 
-  else if(playerSelection==="PAPER"&&computerSelection==="Rock"){
-    return "You win! Paper beats Rock";
+    else if(playerSelection==="PAPER"&&computerSelection==="Rock"){
+      return "You win! Paper beats Rock";
   }
 
-  else if(playerSelection==="SCISSORS"&&computerSelection==="Rock"){
-    return "You lose! Rock beats Scissors"
+    else if(playerSelection==="SCISSORS"&&computerSelection==="Rock"){
+      return "You lose! Rock beats Scissors"
   }
 
-  else if(playerSelection==="ROCK"&&computerSelection==="Scissors"){
-    return "You win! Rock beats Scissors"
+    else if(playerSelection==="ROCK"&&computerSelection==="Scissors"){
+      return "You win! Rock beats Scissors"
   }
 
-  else if(playerSelection==="PAPER"&&computerSelection==="Scissors"){
-    return "You lose! Scissors beats paper";
+    else if(playerSelection==="PAPER"&&computerSelection==="Scissors"){
+      return "You lose! Scissors beats paper";
   }
 
-  else if(playerSelection==="SCISSORS"&&computerSelection==="Paper"){
-    return "You win! Scissors beats paper"
+    else if(playerSelection==="SCISSORS"&&computerSelection==="Paper"){
+      return "You win! Scissors beats paper"
   }
-  else if(playerSelection==="ROCK"&&computerSelection==="Rock"){
-    return "It's a tie! Rock cannot beat Rock";
+    else if(playerSelection==="ROCK"&&computerSelection==="Rock"){
+      return "It's a tie! Rock cannot beat Rock";
   }
-  else if(playerSelection==="SCISSORS"&&computerSelection==="Scissors"){
-     return "It's a tie! Scissors cannot beat Scissors";
+    else if(playerSelection==="SCISSORS"&&computerSelection==="Scissors"){
+      return "It's a tie! Scissors cannot beat Scissors";
   }   
-  else if(playerSelection==="PAPER"&&computerSelection==="Paper"){
-    return "It's a tie! Paper cannot beat Paper";
-  }
- else{
-    return "Wrong Input! Try again";
+    else if(playerSelection==="PAPER"&&computerSelection==="Paper"){
+      return "It's a tie! Paper cannot beat Paper";
+  }   
+    else{
+      return "Wrong Input! Try again";
   }
 }
-//console.log(playRound(getPlayerChoice(),getComputerChoice()));
+
 
 function game(){
-  let player=0;
-    let computer=0;
+  let totalPlayerWins=0;
+  let totalComputerWins=0;
+
   for (let i=0;i<=4;i++){
     let result=playRound(getPlayerChoice(),getComputerChoice());
     console.log(result)
+
     if(result.includes("win")){
-      player++;
+      totalPlayerWins++;
     }
     else if(result.includes("lose")){
-      computer++;
+      totalComputerWins++;
     }
   }
 
-  if(player>computer){
+  if(totalPlayerWins>totalComputerWins){
     console.log("Hurray! Overall youa re the winner");
   }
-  else if(computer>player){
+  else if(totalComputerWins>totalPlayerWins){
     console.log("Try again. Game over")
   }
   else{
     console.log("It's a tie");
   }
 }
+
 game();
 
