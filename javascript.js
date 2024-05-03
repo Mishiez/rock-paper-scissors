a8bb348f5e6350079df63fe29b74db9089b0ce48
Mishiez <michellegathonimain@gmail.com>
2024-05-03 18:22:31 +0300

    Create a function to play one round of the game

--- javascript.js
+++ javascript.js
@@ -2,10 +2,10 @@ let humanScore=0;
 let computerScore=0;
 
 function getComputerChoice(){
-    let computerChoice=["Rock","Paper","Scissors"];
+    let computerChoice=["ROCK","PAPER","SCISSORS"];
     return computerChoice[Math.floor(Math.random()*computerChoice.length)];
 }
-console.log(getComputerChoice());
+//console.log(getComputerChoice());
 
 function getHumanChoice(){
     let humanChoice=prompt("Choose between 'Rock','Paper' or 'Scissors': ");
@@ -18,4 +18,41 @@ function getHumanChoice(){
     }
 }
 
-console.log(getHumanChoice());
+//console.log(getHumanChoice());
+
+
+function playRound(humanChoice,computerChoice){
+    if(humanSelection==="ROCK"&&computerSelection==="PAPER"){
+        computerScore++;
+        return "You lose";
+    }
+    else if(humanSelection==="PAPER"&&computerSelection==="ROCK"){
+        humanScore++;
+        return "You win";
+    }
+    else if(humanSelection==="SCISSORS"&&computerSelection==="PAPER"){
+        humanScore++;
+        return "You win";
+    }
+    else if(humanSelection==="PAPER"&&computerSelection==="SCISSORS"){
+        computerScore++;
+        return "You lose";
+    }
+    else if(humanSelection==="ROCK"&&computerSelection==="SCISSORS"){
+        humanScore++;
+        return "You win";
+    }
+    else if(humanSelection==="SCISSORS"&&computerSelection==="ROCK"){
+        computerScore++;
+        return "You lose";
+    }
+    else{
+        return "You tie";
+    }
+    
+}
+const humanSelection=getHumanChoice();
+const computerSelection=getComputerChoice();
+
+console.log(playRound(humanSelection,computerSelection));
+console.log(humanScore);
