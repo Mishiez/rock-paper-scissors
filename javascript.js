4e2928f9ab60b4627937a149f9cf5e4f0be95798
Mishiez <michellegathonimain@gmail.com>
2024-05-03 12:33:58 +0300

    Create a function to get the human choice

--- javascript.js
+++ javascript.js
@@ -1,5 +1,18 @@
 function getComputerChoice(){
     let computerChoice=["Rock","Paper","Scissors"];
-    return computerChoice[Math.floor(Math.random()*computerChoice.length)]
+    return computerChoice[Math.floor(Math.random()*computerChoice.length)];
 }
 console.log(getComputerChoice());
+
+function getHumanChoice(){
+    let humanChoice=prompt("Choose between 'Rock','Paper' or 'Scissors': ");
+    humanChoice=humanChoice.toUpperCase();
+    if(humanChoice==="ROCK"||humanChoice==="PAPER"||humanChoice==="SCISSORS"){
+        return humanChoice;
+    }
+    else{
+        return "Please enter the correct choice.";
+    }
+}
+
+console.log(getHumanChoice());
